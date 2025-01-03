18d406d7931add1ed643c7362c3fa9de5dc492c8
Saharat Thongin <was.dew4546@gmail.com>
2025-01-03 15:51:43 +0700

    Setup env and nestjs config
    - setup env for local development
    - setup api prefix and versioning

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { VersioningType } from '@nestjs/common';
+
+if (process.env.NODE_ENV === 'local') {
+  config({ path: 'src/environment/.env.local' });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+
+  app.setGlobalPrefix('/api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+
+    defaultVersion: '1',
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
